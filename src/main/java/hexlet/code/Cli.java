d052b53bef915ddb11789d82ab3ddb1957714783
madseal7 <vlad.svistunov.75@mail.ru>
2024-08-13 23:14:48 +0300

    step 5

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -1,10 +1,11 @@
 package hexlet.code;
-
+import java.util.Random;
 import java.util.Scanner;
 
 public class Cli {
-    public static void getUserName(){
+    public static void greetUser() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
         System.out.print("May i have your name? ");
         String name = scanner.nextLine();
         System.out.print("Hello, " + name + "!");
--- src/main/java/hexlet/code/Main.java
+++ src/main/java/hexlet/code/Main.java
@@ -1,8 +1,26 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.getUserName();
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.\n" +
+                "1 - Greet\n" +
+                "2 - Even\n" +
+                "0 - Exit\n" +
+                "Your choice: ");
+        int choice = console.nextInt();
+        switch (choice){
+            case 1:
+                Cli.greetUser();
+                break;
+            case 2:
+                EvenGame.start();
+                break;
+            case 0:
+                break;
+        }
     }
 }
